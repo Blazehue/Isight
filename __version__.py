8f0b643dbc6bf5750bb556a62eb9e3f93e84c989
Blazehue <blazehue@users.noreply.github.com>
2026-01-04 13:42:28 +0530

    Add version module

---
+++ __version__.py
@@ -0,0 +1,17 @@
+"""
+Version information for ISight
+
+Author: Blazehue
+Date: January 2026
+"""
+
+__version__ = '1.0.0'
+__version_info__ = (1, 0, 0)
+__author__ = 'Blazehue'
+__author_email__ = 'blazehue@users.noreply.github.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2026 Blazehue'
+__url__ = 'https://github.com/Blazehue/Isight'
+__description__ = 'High-accuracy sign language detection with MediaPipe'
+__title__ = 'ISight'
+__package_name__ = 'isight'
